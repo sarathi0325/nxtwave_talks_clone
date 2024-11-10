ec0f6acaa83be3d23225c56ae09a95f5d9763e1d
sarathi <sarathi@sarathis-MacBook-Air.local>
2024-11-10 12:16:35 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,517 @@
+let myArray=[
+    trendingBtnObj={
+        image1:"https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/podcast/rare-collection-of-insider-knowledge/sandeep.png",
+        headingText1:"An Engineering Manager at Uber Teaches Programmin",
+        duration1:"45:17 Mins",
+        name1:"Sandeep Gorthi",
+        occupation1:"Engineering Manager at Uber",
+
+        image2:"https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/podcast/rare-collection-of-insider-knowledge/pritesh.png",
+        headingText2:"A Sr. Recruiter at Amazon Teaches Acing Interviews",
+        duration2:"41.07 Mins",
+        name2:"Pritesh Malode",
+        occupation2:"Senior Recruiter at Amazon",
+
+        image3:"https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/podcast/rare-collection-of-insider-knowledge/sanjay.png",
+        headingText3:"A Global CEO Teaches Mastering Time Management",
+        duration3:"56:32 Mins",
+        name3:"Sanjay Sehgal",
+        occupation3:"Chairman & CEO at Msys Technologies"
+    },
+
+    allBtnObj={
+        image1:"https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/podcast/rare-collection-of-insider-knowledge/goutam.png",
+        headingText1:"An Engineer at Meta Teaches You About Designing Technology",
+        duration1:"50:46 Mins",
+        name1:"Goutam Reddy",
+        occupation1:"Platform Security Engineer Meta USA",
+
+        image2:"https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/podcast/rare-collection-of-insider-knowledge/ashok-vardhan.png",
+        headingText2:"Software Engineer at Google Teaches Google Mindset",
+        duration2:"1:23:53 Mins",
+        name2:"Ashok Vardhan Viswanadha",
+        occupation2:"Staff Software Engineer at Google",
+
+        image3:"https://nxtwave.imgix.net/ccbp-website/podcast/rare-collection-of-insider-knowledge/sri-vidhya.png?auto=format,compress&q=80",
+        headingText3:"A Scientist at Apple Teaches You About Machine Learning",
+        duration3:"41:27 Mins",
+        name3:"Sri Vidya Pranavi", 
+        occupation3:"Machine Learning Scientist at Apple"
+    },
+
+    techBtnObj={
+        image1:"https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/podcast/rare-collection-of-insider-knowledge/goutam.png",
+        headingText1:"An Engineer at Meta Teaches You About Designing Technology",
+        duration1:"50:46 Mins",
+        name1:"Goutam Reddy",
+        occupation1:"Platform Security Engineer Meta USA",
+
+        image2:"https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/podcast/rare-collection-of-insider-knowledge/abhinav.png",
+        headingText2:"A Software Engineer at Twitter Teaches Coder's Mindset",
+        duration2:"50:36 Mins",
+        name2:"Abhinav Reddy",
+        occupation2:"Software Development Engineer, Twitter",
+
+        image3:"https://nxtwave.imgix.net/ccbp-website/podcast/rare-collection-of-insider-knowledge/nikhil.png?auto=format,compress&q=80",
+        headingText3:"A Software Engineer at Uber Teaches You About Coding",
+        duration3:"14:18 Mins",
+        name3:"Nikhil VAVS",
+        occupation3:"Software Engineer at Uber"
+
+    },
+
+    careerBtnObj={
+        image1:"https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/podcast/rare-collection-of-insider-knowledge/deepa-wadhwani.png",
+        headingText1:"HR Lead of ADP Teaches Mastering Interviews",
+        duration1:"31:09 Mins",
+        name1:"Deepa Wadhwani",
+        occupation1:"Director - Lead Acquisition of ADP",
+
+        image2:"https://nxtwave.imgix.net/ccbp-website/podcast/rare-collection-of-insider-knowledge/hari-tn.png?auto=format,compress&q=80",
+        headingText2:"The HR Head of Big Basket Teaches You About Building a Career",
+        duration2:"43:19 Mins",
+        name2:"Hari TN",
+        occupation2:"Head HR, Big Basket",
+
+        image3:"https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/podcast/rare-collection-of-insider-knowledge/siva-ghani-reddy.png",
+        headingText3:"A Sr. Manager at Microsoft Teaches  Product Management",
+        duration3:"01:01:13 Mins",
+        name3:"Siva Ghani Reddy",
+        occupation3:"Senior Product Manager at Microsoft"
+
+    },
+
+    cxoBtnObj={
+        image1:"https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/podcast/rare-collection-of-insider-knowledge/shiva-kumar.png",
+        headingText1:"A Chief Technology Officer at Slice Teaches  Leadership",
+        duration1:"01:06:52 Mins",
+        name1:"Shiva Kumar",
+        occupation1:"Chief Technology Officer at Slice",
+
+        image2:"https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/podcast/rare-collection-of-insider-knowledge/sanjay-sehgal.png",
+        headingText2:"A CEO at Msys Teaches Innovation & Entrepreneurship",
+        duration2:"49:07 Mins",
+        name2:"Sanjay Sehgal",
+        occupation2:"Chairman & CEO at Msys Technologies",
+
+        image3:"https://nxtwave.imgix.net/ccbp-website/podcast/rare-collection-of-insider-knowledge/rakesh-mishra.png?auto=format,compress&q=80",
+        headingText3:"The Co-founder of Uhana Teaches You About 5G",
+        duration3:"48:49 Mins",
+        name3:"Rakesh Mishra",
+        occupation3:"Co-founder of Uhana (Acquired by VMware), PhD at Stanford University"
+
+    },
+
+    productivityBtnObj={
+        image1:"https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/podcast/rare-collection-of-insider-knowledge/kiran.png",
+        headingText1:"An Engineer at Google Teaches Thriving in Tech & Life",
+        duration1:"54:27 Mins",
+        name1:"Sai Kiran Gorthi",
+        occupation1:"Software Engineer at Google",
+
+        image2:"https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/podcast/rare-collection-of-insider-knowledge/sanjay.png",
+        headingText2:"A Global CEO Teaches Mastering Time Management",
+        duration2:"56:32 Mins",
+        name2:"Sanjay Sehgal",
+        occupation2:"Chairman & CEO at Msys Technologies",
+
+        image3:"https://cdn.prod.website-files.com/64362e90abbff481a6d25ce8/6690c3b977eb17ab2fe49ddb_krishna-raghavan-thumbnail.webp",
+        headingText3:"Discover what the Top 1% Professionals do differently",
+        duration3:"41:23 mins",
+        name3:"Krishna Raghavan",
+        occupation3:"People and Tech Leader"
+    },
+
+    gsocBtnObj={
+        image1:"https://nxtwave.imgix.net/ccbp-website/podcast/rare-collection-of-insider-knowledge/rahul-garg.png?auto=format,compress&q=80",
+        headingText1:"GSOC Alumuni Teaches You About Cracking GSOC",
+        duration1:"46 Mins",
+        name1:"Rahul Garg",
+        occupation1:"IIIT Hyderabad, GSOC Alumnus",
+
+        image2:"https://nxtwave.imgix.net/ccbp-website/podcast/rare-collection-of-insider-knowledge/nirmal-manoj.png?auto=format,compress&q=80",
+        headingText2:"GSOC Alumuni teaches You About the GSOC Process",
+        duration2:"46 Mins",
+        name2:"Nirmal Manoj",
+        occupation2:"IIIT Hyderabad, GSOC Alumnus",
+
+        image3:"https://nxtwave.imgix.net/ccbp-website/podcast/rare-collection-of-insider-knowledge/sivay-lamba.png?auto=format,compress&q=80",
+        headingText3:"GSOC Mentor Teaches You About Success Strategies for GSOC",
+        duration3:"47 Mins",
+        name3:"Mr. Shivay Lamba",
+        occupation3:"CTO, DarkHorse"
+    },
+
+
+    panelBtnObj={
+        image1:"https://nxtwave.imgix.net/ccbp-website/podcast/rare-collection-of-insider-knowledge/panel-2.png?auto=format,compress&q=80",
+        headingText1:"MAANG Experts Teach You About Securing Dream Jobs",
+        duration1:"1:00:11 Mins",
+        name1:"Expert Panel",
+        occupation1:"Software Engineer at Google",
+
+        image2:"https://nxtwave.imgix.net/ccbp-website/podcast/rare-collection-of-insider-knowledge/panel-1.png?auto=format,compress&q=80",
+        headingText2:"HR Leaders TeachYou About Acing Interviews",
+        duration2:"1:09:54 Mins",
+        name2:"Expert Panel",
+        occupation2:"Chairman & CEO at Msys Technologies",
+
+        image3:"https://nxtwave.imgix.net/ccbp-website/podcast/rare-collection-of-insider-knowledge/panel-3.png?auto=format,compress&q=80",
+        headingText3:"Expert Engineers Teach You About 4.0 Technologies",
+        duration3:"1:00:11 Mins",
+        name3:"Expert Panel",
+        occupation3:"People and Tech Leader"
+    }
+
+];
+
+
+
+let trendingBtnEl=document.getElementById('trendingBtn');
+let allBtnEl=document.getElementById('allBtn');
+let techBtnEl=document.getElementById('techBtn');
+let careerBtnEl=document.getElementById('careerBtn');
+let cxoBtnEl=document.getElementById('cxoBtn');
+let productivityBtnEl=document.getElementById('productivityBtn');
+let gsocBtnEl=document.getElementById('gsocBtn');
+let panelBtnEl=document.getElementById('panelBtn')
+
+
+
+let heading1El=document.getElementById('heading1');
+let heading2El=document.getElementById('heading2');
+let heading3El=document.getElementById('heading3');
+
+let image1El=document.getElementById('image1');
+let image2El=document.getElementById('image2');
+let image3El=document.getElementById('image3');
+
+let duration1El=document.getElementById('duration1');
+let duration2El=document.getElementById('duration2');
+let duration3El=document.getElementById('duration3');
+
+let name1El=document.getElementById('name1');
+let name2El=document.getElementById('name2');
+let name3El=document.getElementById('name3');
+
+let occupation1El=document.getElementById('occupation1');
+let occupation2El=document.getElementById('occupation2');
+let occupation3El=document.getElementById('occupation3');
+
+
+
+function changeContentIntoTrending(){
+    trendingBtnEl.classList.add('bg-color');
+
+    allBtnEl.classList.remove('bg-color');
+    techBtnEl.classList.remove('bg-color');
+    careerBtnEl.classList.remove('bg-color');
+    cxoBtnEl.classList.remove('bg-color');
+    productivityBtnEl.classList.remove('bg-color');
+    gsocBtnEl.classList.remove('bg-color');
+    panelBtnEl.classList.remove('bg-color');
+
+    image1El.src=myArray[0].image1;
+    image2El.src=myArray[0].image2;
+    image3El.src=myArray[0].image3;
+
+    heading1El.textContent=myArray[0].headingText1;
+    heading2El.textContent=myArray[0].headingText2;
+    heading3El.textContent=myArray[0].headingText3;
+
+    duration1El.textContent=myArray[0].duration1;
+    duration2El.textContent=myArray[0].duration2;
+    duration3El.textContent=myArray[0].duration3;
+
+    name1El.textContent=myArray[0].name1;
+    name2El.textContent=myArray[0].name2;
+    name3El.textContent=myArray[0].name3;
+
+    occupation1El.textContent=myArray[0].occupation1;
+    occupation2El.textContent=myArray[0].occupation2;
+    occupation3El.textContent=myArray[0].occupation3;
+}
+
+changeContentIntoTrending();
+
+
+function changeContentIntoAll(){
+    allBtnEl.classList.add('bg-color');
+
+    trendingBtnEl.classList.remove('bg-color');
+    techBtnEl.classList.remove('bg-color');
+    careerBtnEl.classList.remove('bg-color');
+    cxoBtnEl.classList.remove('bg-color');
+    productivityBtnEl.classList.remove('bg-color');
+    gsocBtnEl.classList.remove('bg-color');
+    panelBtnEl.classList.remove('bg-color');
+
+
+    image1El.src=myArray[1].image1;
+    image2El.src=myArray[1].image2;
+    image3El.src=myArray[1].image3;
+
+    heading1El.textContent=myArray[1].headingText1;
+    heading2El.textContent=myArray[1].headingText2;
+    heading3El.textContent=myArray[1].headingText3;
+
+    duration1El.textContent=myArray[1].duration1;
+    duration2El.textContent=myArray[1].duration2;
+    duration3El.textContent=myArray[1].duration3;
+
+    name1El.textContent=myArray[1].name1;
+    name2El.textContent=myArray[1].name2;
+    name3El.textContent=myArray[1].name3;
+
+    occupation1El.textContent=myArray[1].occupation1;
+    occupation2El.textContent=myArray[1].occupation2;
+    occupation3El.textContent=myArray[1].occupation3;
+
+}
+
+
+function changeContentIntoTech(){
+  
+    productivityBtnEl.classList.remove('bg-color');
+    allBtnEl.classList.remove('bg-color');
+    trendingBtnEl.classList.remove('bg-color');
+    techBtnEl.classList.add('bg-color');
+    careerBtnEl.classList.remove('bg-color');
+    gsocBtnEl.classList.remove('bg-color');
+    cxoBtnEl.classList.remove('bg-color');
+    panelBtnEl.classList.remove('bg-color');
+
+
+
+
+    image1El.src=myArray[2].image1;
+    image2El.src=myArray[2].image2;
+    image3El.src=myArray[2].image3;
+
+    heading1El.textContent=myArray[2].headingText1;
+    heading2El.textContent=myArray[2].headingText2;
+    heading3El.textContent=myArray[2].headingText3;
+
+    duration1El.textContent=myArray[2].duration1;
+    duration2El.textContent=myArray[2].duration2;
+    duration3El.textContent=myArray[2].duration3;
+
+    name1El.textContent=myArray[2].name1;
+    name2El.textContent=myArray[2].name2;
+    name3El.textContent=myArray[2].name3;
+
+    occupation1El.textContent=myArray[2].occupation1;
+    occupation2El.textContent=myArray[2].occupation2;
+    occupation3El.textContent=myArray[2].occupation3;
+}
+
+
+function changeContentIntoCareer(){
+
+    productivityBtnEl.classList.remove('bg-color');
+    allBtnEl.classList.remove('bg-color');
+    trendingBtnEl.classList.remove('bg-color');
+    techBtnEl.classList.remove('bg-color');
+    careerBtnEl.classList.add('bg-color');
+    gsocBtnEl.classList.remove('bg-color');
+    cxoBtnEl.classList.remove('bg-color');
+    panelBtnEl.classList.remove('bg-color');
+
+
+
+    image1El.src=myArray[3].image1;
+    image2El.src=myArray[3].image2;
+    image3El.src=myArray[3].image3;
+
+    heading1El.textContent=myArray[3].headingText1;
+    heading2El.textContent=myArray[3].headingText2;
+    heading3El.textContent=myArray[3].headingText3;
+
+    duration1El.textContent=myArray[3].duration1;
+    duration2El.textContent=myArray[3].duration2;
+    duration3El.textContent=myArray[3].duration3;
+
+    name1El.textContent=myArray[3].name1;
+    name2El.textContent=myArray[3].name2;
+    name3El.textContent=myArray[3].name3;
+
+    occupation1El.textContent=myArray[3].occupation1;
+    occupation2El.textContent=myArray[3].occupation2;
+    occupation3El.textContent=myArray[3].occupation3;
+}
+
+
+function changeContentIntoCxo(){
+
+    productivityBtnEl.classList.remove('bg-color');
+    allBtnEl.classList.remove('bg-color');
+    trendingBtnEl.classList.remove('bg-color');
+    techBtnEl.classList.remove('bg-color');
+    careerBtnEl.classList.remove('bg-color');
+    gsocBtnEl.classList.remove('bg-color');
+    cxoBtnEl.classList.add('bg-color');
+    panelBtnEl.classList.remove('bg-color');
+
+
+
+
+    image1El.src=myArray[4].image1;
+    image2El.src=myArray[4].image2;
+    image3El.src=myArray[4].image3;
+
+    heading1El.textContent=myArray[4].headingText1;
+    heading2El.textContent=myArray[4].headingText2;
+    heading3El.textContent=myArray[4].headingText3;
+
+    duration1El.textContent=myArray[4].duration1;
+    duration2El.textContent=myArray[4].duration2;
+    duration3El.textContent=myArray[4].duration3;
+
+    name1El.textContent=myArray[4].name1;
+    name2El.textContent=myArray[4].name2;
+    name3El.textContent=myArray[4].name3;
+
+    occupation1El.textContent=myArray[4].occupation1;
+    occupation2El.textContent=myArray[4].occupation2;
+    occupation3El.textContent=myArray[4].occupation3;
+}
+
+
+function changeContentIntoProductivity(){
+
+    productivityBtnEl.classList.add('bg-color');
+    allBtnEl.classList.remove('bg-color');
+    trendingBtnEl.classList.remove('bg-color');
+    techBtnEl.classList.remove('bg-color');
+    careerBtnEl.classList.remove('bg-color');
+    gsocBtnEl.classList.remove('bg-color');
+    cxoBtnEl.classList.remove('bg-color');
+    panelBtnEl.classList.remove('bg-color');
+
+
+
+
+    image1El.src=myArray[5].image1;
+    image2El.src=myArray[5].image2;
+    image3El.src=myArray[5].image3;
+
+    heading1El.textContent=myArray[5].headingText1;
+    heading2El.textContent=myArray[5].headingText2;
+    heading3El.textContent=myArray[5].headingText3;
+
+    duration1El.textContent=myArray[5].duration1;
+    duration2El.textContent=myArray[5].duration2;
+    duration3El.textContent=myArray[5].duration3;
+
+    name1El.textContent=myArray[5].name1;
+    name2El.textContent=myArray[5].name2;
+    name3El.textContent=myArray[5].name3;
+
+    occupation1El.textContent=myArray[5].occupation1;
+    occupation2El.textContent=myArray[5].occupation2;
+    occupation3El.textContent=myArray[5].occupation3;
+}
+
+function changeContentIntoGsoc(){
+
+    productivityBtnEl.classList.remove('bg-color');
+    allBtnEl.classList.remove('bg-color');
+    trendingBtnEl.classList.remove('bg-color');
+    techBtnEl.classList.remove('bg-color');
+    careerBtnEl.classList.remove('bg-color');
+    gsocBtnEl.classList.add('bg-color');
+    cxoBtnEl.classList.remove('bg-color');
+    panelBtnEl.classList.remove('bg-color');
+
+
+
+
+    image1El.src=myArray[6].image1;
+    image2El.src=myArray[6].image2;
+    image3El.src=myArray[6].image3;
+
+    heading1El.textContent=myArray[6].headingText1;
+    heading2El.textContent=myArray[6].headingText2;
+    heading3El.textContent=myArray[6].headingText3;
+
+    duration1El.textContent=myArray[6].duration1;
+    duration2El.textContent=myArray[6].duration2;
+    duration3El.textContent=myArray[5].duration3;
+
+    name1El.textContent=myArray[6].name1;
+    name2El.textContent=myArray[6].name2;
+    name3El.textContent=myArray[6].name3;
+
+    occupation1El.textContent=myArray[6].occupation1;
+    occupation2El.textContent=myArray[6].occupation2;
+    occupation3El.textContent=myArray[6].occupation3;
+}
+
+function changeContentIntoPanel(){
+
+    productivityBtnEl.classList.remove('bg-color');
+    allBtnEl.classList.remove('bg-color');
+    trendingBtnEl.classList.remove('bg-color');
+    techBtnEl.classList.remove('bg-color');
+    careerBtnEl.classList.remove('bg-color');
+    gsocBtnEl.classList.remove('bg-color');
+    cxoBtnEl.classList.remove('bg-color');
+    panelBtnEl.classList.add('bg-color');
+
+
+    image1El.src=myArray[7].image1;
+    image2El.src=myArray[7].image2;
+    image3El.src=myArray[7].image3;
+
+    heading1El.textContent=myArray[7].headingText1;
+    heading2El.textContent=myArray[7].headingText2;
+    heading3El.textContent=myArray[7].headingText3;
+
+    duration1El.textContent=myArray[7].duration1;
+    duration2El.textContent=myArray[7].duration2;
+    duration3El.textContent=myArray[7].duration3;
+
+    name1El.textContent=myArray[7].name1;
+    name2El.textContent=myArray[7].name2;
+    name3El.textContent=myArray[7].name3;
+
+    occupation1El.textContent=""
+    occupation2El.textContent=""
+    occupation3El.textContent=""
+}
+
+
+
+trendingBtnEl.addEventListener('click',function(){
+    changeContentIntoTrending();
+});
+
+allBtnEl.addEventListener('click',function(){
+    changeContentIntoAll();
+});
+
+techBtnEl.addEventListener('click',function(){
+    changeContentIntoTech();
+});
+
+
+careerBtnEl.addEventListener('click',function(){
+    changeContentIntoCareer();
+});
+
+cxoBtnEl.addEventListener('click',function(){
+    changeContentIntoCxo();
+});
+
+productivityBtnEl.addEventListener('click',function(){
+    changeContentIntoProductivity();
+});
+
+gsocBtnEl.addEventListener('click',function(){
+    changeContentIntoGsoc();
+});
+
+
+panelBtnEl.addEventListener('click',function(){
+    changeContentIntoPanel();
+});
